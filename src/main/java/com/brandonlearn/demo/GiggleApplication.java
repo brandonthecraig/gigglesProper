2a0b5e0f1304b6586f32e844affabdbe541b02c4
Brandon Craig <BrandonCraig@rentalcars.com>
2018-10-25 10:31:59 +0100

    Created new maven proj

---
+++ src/main/java/com/brandonlearn/demo/GiggleApplication.java
@@ -0,0 +1,12 @@
+package com.brandonlearn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiggleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiggleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/brandonlearn/demo/GiggleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brandonlearn.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GiggleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
