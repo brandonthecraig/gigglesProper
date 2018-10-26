1ba29f6a89720dc544944ae0c3207022c20f0002
Brandon Craig <BrandonCraig@rentalcars.com>
2018-10-26 10:44:39 +0100

    Dodgy Push
    Just pushing my proj so I can work on the weekend

--- src/main/java/com/brandongo/giggles/controller/GiglistController.java
+++ src/main/java/com/brandongo/giggles/controller/GiglistController.java
@@ -1,13 +1,21 @@
 package com.brandongo.giggles.controller;
 
 
+import com.brandongo.giggles.data.entity.Gig;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GiglistController {
 
     @RequestMapping("/gig_display")
-    public String giglist() { return "GigDisplay";}
+    // added the model thing, use that to enter your list of objects. Allows you to access backend from frontend
+    // HaltCampaign.html is model for this along with HaltController.java
+    public String giglist(Model model)
+    {
+        Gig gigTest = new Gig("XS Malarkey", "Ross", 5);
+        model.addAttribute(gigTest);
+        return "GigDisplay";}
 
 }
